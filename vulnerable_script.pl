14bfadce4741e938048f1e7786d67c71a8bd5f37
Rohit Kumar <rohittourister@gmail.com>
2025-06-09 16:04:37 +0530

    Create vulnerable_script.pl

---
+++ vulnerable_script.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my $user_input = $ARGV[0];
+
+# Vulnerable: user input is passed directly to system()
+# This can be exploited if $user_input is something like: "; rm -rf /"
+system("ls $user_input");
